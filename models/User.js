9e64522da2e12fade7cb23480e5e3801f026c219
Desmond845 <akugbedesmond845@gmail.com>
2026-03-04 11:05:48 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -8,15 +8,14 @@ async function hash () {
 //  return;
 const userSchema = new mongoose.Schema({
 id: { type: Number, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true, set: (v) => v.toLowerCase() },
     email: { type: String, required: true, unique: true },
 
   password: { type: String, required: true },
   lastSeen: { type: Date},
-    // password: { type: String, required: true },
+    
 avatar: {type: String, default: ''},
 bio: {type: String, default: ''}
-  // You can add more fields later: displayName, avatar, etc.
 }, { timestamps: true });
 
 // Hash password before saving
@@ -31,16 +30,6 @@ userSchema.pre('save', async function(next) {
 userSchema.methods.comparePassword = async function(candidate) {
   return await bcrypt.compare(candidate, this.password);
 };
-// $2b$10$c5DmYo1iljp5fhqk6ixOeuh5OnT3bWhVGqB7HQvWzt7yXwg6VGG9W
-//  $2b$10$c5DmYo1iljp5fhqk6ixOeuh5OnT3bWhVGqB7HQvWzt7yXwg6VGG9W
-// In seed script or manually add:
-// {
-//   id: 0000001, // special ID
-//   username: 'ChitChat Official',
-//   email: 'official@chitchat.com',
-//   password: bcrypt.hashSync('some-secure-password', 10),
-//   bio: 'Official announcements and updates',
-//   avatar: '/official-avatar.png'
-// }
+
 const User = mongoose.model('User', userSchema);
 export default User;
